39e8c15a2809712f30367f1239aeebc42785d1ef
Agon20 <azemi.agon20@gmail.com>
2026-04-01 14:42:06 +0200

    ``` Remove build artifacts, fix gitignore ```

--- ${project.build.directory}/classes/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS insurance_application (
-    id                  BIGSERIAL PRIMARY KEY,
-    postal_code         VARCHAR(10)      NOT NULL,
-    annual_mileage      INTEGER          NOT NULL,
-    vehicle_type_factor VARCHAR(20)      NOT NULL,
-    premium             DOUBLE PRECISION NOT NULL,
-    created_date        TIMESTAMP        NOT NULL
-);
